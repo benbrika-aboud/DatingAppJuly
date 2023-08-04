ee114bac4b84e210030fbb9e87dec8ad97f67c2c
aboud <benbrika36@gmail.com>
2023-08-04 10:31:07 +0200

    End of Section 9

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -7,9 +11,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberDetailComponent implements OnInit {
 
-  constructor() { }
+  member: Member| undefined;
+  galleryOptions: NgxGalleryOptions[] = [];
+  galleryImages: NgxGalleryImage[] = [];
+
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMember();
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+
+    
+  }
+
+  getImages() {
+    if(!this.member) return [];
+
+    const imageUrls: { small: string; medium: string; big: string; }[] = [];
+    for(const photo of this.member.photos)
+    {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url
+      })
+    }
+    // this.member.photos.forEach(photo => {
+    //   imageUrls.push({
+    //     small: photo.url,
+    //     medium: photo.url,
+    //     big: photo.url
+    //   })
+    // });
+    return imageUrls;
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    
+    if(!username) return;
+
+    this.memberService.getMember(username)
+    .subscribe({
+      next: member=> {
+        this.member = member;
+        this.galleryImages = this.getImages();
+      }
+    })
   }
 
 }
